514968da1096c51d14932686a86e115fab07d621
jansaridhruvi <dhruvijansari21@gnu.ac.in>
2023-09-10 10:15:42 +0530

    practical-5_036

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "mad_pr5_21012021036"
+include(":app")
