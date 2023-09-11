275664fe3601d5b722e657c237520cf7ed84d8c8
student <student@gnu.ac.in>
2023-09-11 10:12:54 +0530

    practical 6

--- app/src/main/java/com/example/mad_pr5_21012021036/MainActivity.kt
+++ app/src/main/java/com/example/mad_pr5_21012021036/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.example.mad_pr5_21012021036
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var bl : FloatingActionButton =findViewById(R.id.play)
+        bl.setOnClickListener(){
+          playpause()
+        }
+        var b2: FloatingActionButton =findViewById(R.id.stop)
+        b2.setOnClickListener(){
+            stop_m()
+        }
+    }
+    fun playpause()
+    {
+        Intent(applicationContext,MyService::class.java).putExtra(MyService.PLAYERKEY,MyService.PLAYERKEY).apply { startService(this) }
+    }
+    fun stop_m()
+    {
+        Intent(applicationContext,MyService::class.java).putExtra(MyService.PLAYERKEY,MyService.PLAYERKEY).apply { stopService(this) }
     }
 }
